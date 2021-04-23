ff17bcc202c5288952245444cf621df4e825cf93
yaroslavStn <yriksotnikov@gmail.com>
2021-04-23 22:42:16 +0300

    task1-2 done

--- src/human/Student.java
+++ src/human/Student.java
@@ -1,5 +1,7 @@
 package human;
 
+import java.util.Comparator;
+
 public class Student {
 
     private String name;
@@ -10,6 +12,18 @@ public class Student {
         this.name = name;
         this.surname = surname;
         this.age = age;
+    }
+
+    public static Comparator<Student> ComparatorByName() {
+        return new ComparatorByName();
+    }
+
+    public static Comparator<Student> ComparatorByAge() {
+        return new ComparatorByAge();
+    }
+
+    public static Comparator<Student> DefaultComparator() {
+        return new DefaultComparator();
     }
 
     public String getName() {
@@ -34,5 +48,31 @@ public class Student {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    private static class ComparatorByName implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.name.compareTo(o2.name);
+        }
+    }
+
+    private static class ComparatorByAge implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return Integer.compare(o1.age, o2.age);
+        }
+    }
+
+    private static class DefaultComparator implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            if (o1.name.compareTo(o2.name) == 0) {
+                return Integer.compare(o1.age, o2.age);
+            } else return o1.name.compareTo(o2.name);
+        }
     }
 }
