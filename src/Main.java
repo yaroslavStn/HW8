bb7a8c345cad98650c0111480cd334e5932c62b3
yaroslavStn <yriksotnikov@gmail.com>
2021-04-24 19:21:39 +0300

    inner classes

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,116 @@
 import converter.IStudentStringConverter;
-import converter.StudentStringConverterJSON;
-import converter.StudentStringConverterStandard;
-import converter.StudentStringConverterXML;
 import human.Student;
 import printer.IStudentPrinter;
-import printer.StudentPrinterConsole;
-import printer.StudentPrinterDefault;
-import printer.StudentPrinterDelegate;
 
 public class Main {
-    public static void main (String[] args) {
-    Main.run();
+    public static void main(String[] args) {
+        Main.run();
     }
 
     private static void run() {
-        Student student1 = new Student("Vasia", "Pupkin",20 );
-        IStudentStringConverter converterJSON = new StudentStringConverterJSON();
-        IStudentStringConverter converterXML = new StudentStringConverterXML();
-        IStudentStringConverter converterStandard = new StudentStringConverterStandard();
-        IStudentStringConverter [] converters ={
+        Student student1 = new Student("Vasia", "Pupkin", 20);
+        IStudentStringConverter converterJSON = new IStudentStringConverter() {
+            @Override
+            public String convert(Student student) {
+                StringBuilder result = new StringBuilder();
+                result.append("{\n");
+                result.append("\"name\" : \"");
+                result.append(student.getName());
+                result.append("\",\n");
+                result.append("\"surname\" : \"");
+                result.append(student.getSurname());
+                result.append("\",\n");
+                result.append("\"age\" : \"");
+                result.append(student.getAge());
+                result.append("\"\n}");
+
+                return result.toString();
+            }
+        };
+
+
+        IStudentStringConverter converterXML = new IStudentStringConverter() {
+            @Override
+            public String convert(Student student) {
+                StringBuilder result = new StringBuilder();
+                result.append("<student>\n");
+                result.append("<name>");
+                result.append(student.getName());
+                result.append("</name>\n");
+                result.append("<surname>");
+                result.append(student.getSurname());
+                result.append("</surname>\n");
+                result.append("<age>");
+                result.append(student.getAge());
+                result.append("</age>\n");
+                result.append("</student>");
+
+                return result.toString();
+            }
+        };
+
+        IStudentStringConverter converterStandard = new IStudentStringConverter() {
+            @Override
+            public String convert(Student student) {
+                StringBuilder result = new StringBuilder();
+                result.append("student:\n");
+                result.append("name=");
+                result.append(student.getName() + '\n');
+                result.append("surname=");
+                result.append(student.getSurname() + '\n');
+                result.append("age=");
+                result.append(student.getAge());
+                result.append('\n');
+
+                return result.toString();
+            }
+        };
+
+        IStudentStringConverter[] converters = {
                 converterJSON,
                 converterStandard,
                 converterXML
         };
 
         for (IStudentStringConverter converter : converters) {
-            IStudentPrinter printerConsole = new StudentPrinterConsole(converter);
+            IStudentPrinter printerConsole = new IStudentPrinter() {
+                @Override
+                public void print(Student student) {
+                    System.out.println(converter.convert(student));
+                }
+            };
             printerConsole.print(student1);
         }
 
-        IStudentPrinter printerConsole= new StudentPrinterConsole(converters[0]);
-        IStudentPrinter printerDefault = new StudentPrinterDefault();
+        IStudentPrinter printerConsole = new IStudentPrinter() {
+            @Override
+            public void print(Student student) {
+                System.out.println(converterStandard.convert(student));
+            }
+        };
+        printerConsole.print(student1);
+        IStudentPrinter printerDefault = new IStudentPrinter() {
+            @Override
+            public void print(Student student) {
+
+            }
+        };
         printerDefault.print(student1);
+
         IStudentPrinter[] printers = {
                 printerConsole,
                 printerDefault
         };
-        IStudentPrinter printerDelegate = new StudentPrinterDelegate(printers);
-        printerDelegate.print(student1);
+
+        IStudentPrinter printerDelegate = new IStudentPrinter() {
+            @Override
+            public void print(Student student) {
+                for (IStudentPrinter printer : printers) {
+                    printer.print(student);
+                }
+            }
+        };
+       printerDelegate.print(student1);
 
     }
 }
